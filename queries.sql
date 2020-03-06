37db434b38f8978e524d747a193889dafe503d30
tarbielee <tarbieletsoalo@gmail.com>
2020-03-06 15:05:22 +0200

    refactored-code

--- queries.sql
+++ queries.sql
@@ -1,2 +1,14 @@
+SELECT * from customer
+
+SELECT firstname From customer;
+
+SELECT * FROM customer WHERE customerid = 1;
+
+UPDATE customer
+SET firstname = 'Lerato', lastname = 'Mabitso'
+WHERE customerid = 1;
+
+DELETE FROM customer
+WHERE customerid = 2;
 
 
