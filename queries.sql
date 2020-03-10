e41b398dec76e73c5c5bb5c8f20fa4d23e704289
tarbielee <tarbieletsoalo@gmail.com>
2020-03-10 16:17:44 +0200

    refactored-code

--- queries.sql
+++ queries.sql
@@ -11,6 +11,20 @@ WHERE customerid = 1;
 DELETE FROM customer
 WHERE customerid = 2;
 
---
+SELECT count(status) FROM orders;
+
+SELECT MAX(amount) FROM payments;
+
+SELECT * FROM customer ORDER BY country ASC;
+
+SELECT * FROM products WHERE buyprice BETWEEN 'R100' AND 'R600';
+
+SELECT * FROM customer WHERE country ='Germany' and city ='Berlin';
+
+SELECT * FROM customer WHERE city ='Cape Town' OR city ='Durban';
+
+SELECT * FROM products WHERE buyprice > 'R500';
+
+SELECT count( * ) FROM orders WHERE status = 'Shipped';
 
 
